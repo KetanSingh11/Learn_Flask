36cf9f06d553ccac41fd24fdebe18fc48d1c46e4
Ketan Singh <ketansingh_vsec@yahoo.com>
2018-04-16 02:25:09 +0530

    authentication and logging - 2

--- items_api.py
+++ items_api.py
@@ -1,34 +1,49 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
 
 app = Flask(__name__)
+app.secret_key = "ketan@&*$singh"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)	# /auth
 
 items = []
 
 
 class Item(Resource):
-	"""
-	docstring for Item
-	"""
-	def get(self):
-		return "Hello World"
-
+	"""	docstring for Item	"""
+	@jwt_required()
 	def get(self, name):
-		for item in items:
-			if item["name"] == name:
-				return item
-		return {'item': None}, 404
-
+		# for item in items:
+		# 	if item["name"] == name:
+		# 		return item
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		return {'item': item}, 200 if item is not None else 404
 
 	def post(self, name):
-		item = {"name": name, "price": 43.99}
+		if next(filter(lambda x: x['name'] == name, items), None) is not None:
+			return {"message": "Item with same name already exists"}, 400  
+
+		data = request.get_json()
+		item = {"name": name, "price": data['price']}
 		items.append(item)
 		return item, 201
 
 
+class ItemList(Resource):
+	"""docstring for ItemList"""
+	def get(self):
+		return {"items": items}
+
+		
 
 api.add_resource(Item, "/item/<string:name>", "/")
+api.add_resource(ItemList, "/items")
+
 
 if __name__=='__main__':
 	app.run(debug=True)
